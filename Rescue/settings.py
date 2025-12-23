6d37d21b5811381e71b9ef832ecccb23c2e5c6ad
nltdat <nltdat.it@gmail.com>
2025-12-23 21:14:23 +0700

    Allow host

--- Rescue/settings.py
+++ Rescue/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-u9lqcigjj@6_e8)0lwx*hau^5r@ogq_$m)du*okde!^&%0czng
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
